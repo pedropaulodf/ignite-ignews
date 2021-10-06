9b4d0cd6ae5b05fe783bdcf8413053156656a79a
pedropaulodf <pedropaulotj@gmail.com>
2021-10-06 15:30:09 -0300

    1/3 - 2/3 Componentes e páginas - done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
 
 import '../styles/global.scss';
 
 // o _app RODA TODA VEZ QUE ALGUÉM CLICA NUMA ROTA
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header/>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
