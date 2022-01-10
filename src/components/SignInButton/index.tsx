5512df0ce18490de77b9861cc0defd15e961e08e
Pedro Paulo <pedropaulotj@gmail.com>
2022-01-09 22:08:34 -0300

    2 - Usando FaunaDB - done

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -6,9 +6,6 @@ import styles from "./styles.module.scss";
 
 export function SignInButton() {
   const { data: session } = useSession();
-
-  console.log(session);
-
   return session ? (
     <button className={styles.signInButton} type="button" onClick={() => signOut()}>
       <FaGithub color="#04d361" />
