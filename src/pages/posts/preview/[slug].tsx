def2b24e3e3081adbf6cdd7bd220247e167fccd7
Pedro Paulo <pedropaulotj@gmail.com>
2022-01-27 21:19:29 -0300

    fix

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -60,7 +60,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
     paths: [],
     fallback: 'blocking'
     // true: Quando usuario acessa o post pela primeira vez, faz a requisicao em tempo real. Mas espera a página carregar para mostrar para o usuario. (Causa Layout Shift-carrega a página sem o conteúdo e depois preenche ele) | Não é bom para SEO.
-    
+
     // false: Se o post não foi gerado ainda, manda o erro de 404.
 
     // 'blocking': Parece o true, mas carrega o post no serverSide e só mostra a tela depois que carregou o conteúdo de fato.
